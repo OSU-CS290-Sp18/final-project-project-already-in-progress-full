f2934462e87261d2e53ae63f420b87e51e903d6e
Chubbard022 <Chubbard02@gmail.com>
2018-06-04 14:50:00 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+function createMap(){
+
+document.getElementById("modal-backdrop").classList.remove("hidden");
+
+document.getElementById("create-modal").style.display ="block";
+
+}
+
+function CloseModal(){
+
+document.getElementById("modal-backdrop").classList.add("hidden");
+
+document.getElementById("create-modal").style.display ="none";
+
+document.getElementById("text-input").value = "";
+
+document.getElementById("attribution-input").value = "";
+
+}
+
+
+//this fuction will create a map when create button is pressed.
+function createMaping(){
+*
+*
+*
+*
+*
+*
+*
+
+}
+
+//this fuction will help the search bar search through all maps
+function search(){
+*
+*
+*
+*
+*
+*
+*
+}
