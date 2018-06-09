7c8ca0ab6744d13aeb3970c4c120835082023464
Crusadifix <Darsken.McLoud@gmail.com>
2018-06-09 11:05:18 -0700

    Begin to modify server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-
-
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoGBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" +
+	mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":"
+	+ mongoPort + "/" + mongoDBName;
+
+var mongoDB = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -18,6 +28,8 @@ app.get('*', function (req, res, next) {
 });
 
 app.use(express.static('public'));
+
+ // Add pages to render here
 
 app.get('*', function (req, res) {
 	res.status(404).render('404Page');
