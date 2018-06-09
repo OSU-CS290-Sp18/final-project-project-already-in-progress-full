79bd2eaca4401e9e77151b91107381f04017dadd
Chubbard022 <Chubbard02@gmail.com>
2018-06-09 14:25:13 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//this function opens modal when clicked.
 function createMap(){
 
 document.getElementById("modal-backdrop").classList.remove("hidden");
@@ -5,7 +6,7 @@ document.getElementById("modal-backdrop").classList.remove("hidden");
 document.getElementById("create-modal").style.display ="block";
 
 }
-
+//this function closes modal when clicked
 function CloseModal(){
 
 document.getElementById("modal-backdrop").classList.add("hidden");
@@ -21,13 +22,21 @@ document.getElementById("attribution-input").value = "";
 
 //this fuction will create a map when create button is pressed.
 function createMaping(){
-*
-*
-*
-*
-*
-*
-*
+  
+  var author = document.getElementById("attribution-input").value;
+
+var conent = document.createElement('article');
+
+  if(map=='' || name==''){
+      alert('you did not add a map name or create a map!');
+      
+  }else{
+
+    *
+    *
+    *
+    *
+    *
 
 }
 
